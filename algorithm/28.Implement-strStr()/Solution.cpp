21192a47be9957075a1ea89f18886a673e693ec1
ghost461 <lingmilch@sina.com>
2017-07-30 20:14:34 +0800

    Implement-strStr()

---
+++ algorithm/28.Implement-strStr()/Solution.cpp
@@ -0,0 +1,30 @@
+/*
+ * 
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. 
+ * 
+ */
+
+#include<string>
+using namespace std;
+class Solution
+{
+	public:
+		int strStr(string haystack, string needle)
+		{
+			int m = haystack.length(), n = needle.length();
+			if(!n)
+				return 0;
+			for(int i = 0; i < m - n + 1; i++)
+			{
+				int j = 0;
+				for(; j < n; j++)
+					if(haystack[i + j] != needle[j])
+						break;
+				if (j == n)
+					return i;
+			}
+			return -1;
+		}
+};
