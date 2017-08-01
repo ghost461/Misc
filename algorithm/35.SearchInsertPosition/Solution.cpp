f52c5cfdea0ba5ee71b314dcf8ec3ba114a36a6d
ghost461 <lingmilch@sina.com>
2017-08-01 10:44:38 +0800

    SearchInsertPosition

---
+++ algorithm/35.SearchInsertPosition/Solution.cpp
@@ -0,0 +1,30 @@
+/*
+ * 
+ * Given a sorted array and a target value, return the index if the target is found. 
+ * If not, return the index where it would be if it were inserted in order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Here are few examples.
+ * [1,3,5,6], 5 -> 2
+ * [1,3,5,6], 2 -> 1
+ * [1,3,5,6], 7 -> 4
+ * [1,3,5,6], 0 -> 0
+ * 
+ */
+
+#include<vector>
+using namespace std;
+class Solution
+{
+	public:
+		int searchInsert(vector<int>& nums, int target)
+		{
+			int i = 0;
+			while(target > nums[i] && i < nums.size())
+			{
+				i++;
+			}
+			return i;
+		}
+};
